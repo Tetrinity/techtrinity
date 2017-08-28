d296808ae2e12f5fe182a4a978515d51cc3e7e01
Sam Gibbon <zephyrsg@hotmail.co.uk>
2017-08-28 13:27:37 +0100

    removed unused code

--- src/main/frame/Footer.jsx
+++ src/main/frame/Footer.jsx
@@ -3,20 +3,12 @@ import moment from 'moment';
 
 import Copyright from './Copyright';
 
-const baseYear = "2017";
-
 class Footer extends React.PureComponent {
 
     constructor(props){
         super(props);
 
         this.state = {}
-    }
-
-    getCopyrightNotice(){
-        let currentYear = moment().year();
-
-        return "Copyright Sam Gibbon, " + baseYear + " - " + currentYear;
     }
 
     render(){
