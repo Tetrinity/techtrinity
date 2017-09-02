2adbeba582786856ea2a526088468e57c34effe6
Sam Gibbon <zephyrsg@hotmail.co.uk>
2017-09-02 18:49:36 +0100

    Using React-Bootstrap for menu

--- src/main/frame/Header.jsx
+++ src/main/frame/Header.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 const Header = () => (
-    <div>
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-        </ul>
-    </div>
+    <Navbar>
+        <Navbar.Header>
+            <Navbar.Brand>
+                <a href="/">Techtrinity</a>
+            </Navbar.Brand>
+            <Navbar.Toggle/>
+        </Navbar.Header>
+        <Nav>
+            <NavItem eventKey={1} href="/">Home</NavItem>
+            <NavItem eventKey={2} href="/about">About</NavItem>
+        </Nav>
+    </Navbar>
 )
 
 export default Header;
