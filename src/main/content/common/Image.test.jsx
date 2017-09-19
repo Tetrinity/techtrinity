066dd600dd2b458c5b88ac251716bc626c34097f
Sam Gibbon <zephyrsg@hotmail.co.uk>
2017-09-19 21:05:11 +0100

    added unit tests for Image

--- src/main/content/common/Image.test.jsx
+++ src/main/content/common/Image.test.jsx
@@ -23,4 +23,24 @@ describe("Image", () => {
     it("should not render if the file name has not been provided", () => {
         expect(image().find("div").length).toEqual(0);
     })
+
+    it("should not render if the date has not been specified for a non-static file", () => {
+        props.name = "example.png";
+
+        expect(image().find("div").length).toEqual(0);
+    })
+
+    it("should render if the date has not been specified for a static file", () => {
+        props.static = true;
+        props.name = "example.png";
+
+        expect(image().find("div").length).toEqual(1);
+    })
+    
+    it("should render if the date has been specified for a non-static file", () => {
+        props.date = "2017-05-11";
+        props.name = "example.png";
+
+        expect(image().find("div").length).toEqual(1);
+    })
 })
