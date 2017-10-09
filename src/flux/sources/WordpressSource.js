bc84c5b19a33967b8e21cef46ff62552a2f434d4
Sam Gibbon <zephyrsg@hotmail.co.uk>
2017-10-09 22:37:44 +0100

    Updated URL with new domain name

--- src/flux/sources/WordpressSource.js
+++ src/flux/sources/WordpressSource.js
@@ -1,6 +1,6 @@
 import Api from './BaseApi';
 
-let wordpressUrl = "http://ec2-18-221-192-144.us-east-2.compute.amazonaws.com";
+let wordpressUrl = "http://www.techtrinity.co.uk/blog";
 
 let pagesEndPoint = wordpressUrl + "/wp-json/wp/v2/pages"
 let postsEndPoint = wordpressUrl + "/wp-json/wp/v2/posts"
